bfeebec4959c8bbc8ab0ffdca818f4fc37dcf4da
Muskaanpal05 <palmuskaan5@gmail.com>
2020-11-01 13:09:50 +0530

    Final Contact Card

--- src/index.js
+++ src/index.js
@@ -1,39 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import contacts from "./contact";
 
 function Card(props) { //passing props as an object into the funcion component containg input properties inside
-    return ( < div >
-        <
-        h2 > { props.name } < /h2>  <
-        img src = { props.img }
-        alt = "avatar_img" /
-        >
-        <
-        p > { props.phone } < /p>  { /* Calling the propertiesof an object called props */ } <
-        p > { props.mail } < /p> <
-        /div>);
-    }
-
-    ReactDOM.render( <
+    return ( <
         div >
         <
-        h1 > My Contacts < /h1>
-
+        div className = "card" >
         <
-        Card name = "Beyonce"
-        img = "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-        phone = "+123 456 789"
-        mail = "b@beyonce.com" / >
+        div className = "top" >
         <
-        Card name = "Jack Bauer"
-        img = "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-        phone = "+987 654 321"
-        mail = "jack@nowhere.com" / >
+        h2 className = "name" > { props.name } < /h2> <
+        img className = "circle-img"
+        src = { props.img }
+        alt = "avatar_img" / >
         <
-        Card name = "Chuck Norris"
-        img = "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-        phone = "+918 372 574"
-        mail = "gmail@chucknorris.com" / > { /* Making custom attributes for custom component which are called props*/ } <
-        /div>,
-        document.getElementById("root")
+        /div> <
+        div className = "bottom" >
+        <
+        p className = "info" > { props.phone } < /p> <
+        p className = "info" > { props.mail } < /p> <
+        /div> <
+        /div> <
+        /div>
     );
+}
+
+ReactDOM.render( <
+    div >
+    <
+    h1 > My Contacts < /h1>
+
+    <
+    Card name = { contacts[0].name }
+    img = { contacts[0].imgURL }
+    phone = { contacts[0].phone }
+    mail = { contacts[0].email }
+    />  <
+    Card name = { contacts[1].name }
+    img = { contacts[1].imgURL }
+    phone = { contacts[1].phone }
+    mail = { contacts[1].email }
+    />  <
+    Card name = { contacts[2].name }
+    img = { contacts[2].imgURL }
+    phone = { contacts[2].phone }
+    mail = { contacts[2].email }
+    />  { /* Making custom attributes for custom component which are called props*/ } <
+    /div>,
+    document.getElementById("root")
+);
